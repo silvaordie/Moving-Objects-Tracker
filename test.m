9c8e1559251fed5739de491804fa39d7daab490c
silvaordie <40876673+silvaordie@users.noreply.github.com>
2018-11-29 22:31:28 +0000

    xyz

--- test.m
+++ test.m
@@ -1,43 +1,37 @@
-clear;
+  clear;
 
 d=dir('*.jpg');
 dd=dir('images*.mat');
 
+load cameraparametersAsus.mat
 imgseq1.rgb=d;
 imgseq1.depth=dd;
 
-frame = track3D_part1( imgseq1,   1 );
+frame = track3D_part1( imgseq1,   'cameraparametersAsus.mat' );
  
 %%
 figure;
 for f=1:length(imgseq1.depth)
     load(imgseq1.depth(f).name)
     hold on;
+        
     for o=1:length(frame(f).obj)
-        if(~isnan(frame(f).obj(o).x))
-            x1=frame(f).obj(o).x(frame(f).obj(o).maxx);
-            y1=frame(f).obj(o).y(frame(f).obj(o).maxx);
-            z1=double(depth_array(x1,y1))/1000;
+        if(~isnan(frame(f).obj(o).xyz))
+            p1=frame(f).obj(o).xyz(:,frame(f).obj(o).maxx);
 
-            x2=frame(f).obj(o).x(frame(f).obj(o).maxy);
-            y2=frame(f).obj(o).y(frame(f).obj(o).maxy);
-            z2=double(depth_array(x2,y2))/1000;
+            p2=frame(f).obj(o).xyz(:,frame(f).obj(o).maxy);
 
-            x3=frame(f).obj(o).x(frame(f).obj(o).minx);
-            y3=frame(f).obj(o).y(frame(f).obj(o).minx);
-            z3=double(depth_array(x3,y3))/1000;
+            p3=frame(f).obj(o).xyz(:,frame(f).obj(o).minx);
 
-            x4=frame(f).obj(o).x(frame(f).obj(o).miny);
-            y4=frame(f).obj(o).y(frame(f).obj(o).miny);
-            z4=double(depth_array(x4,y4))/1000;
+            p4=frame(f).obj(o).xyz(:,frame(f).obj(o).miny);
 
-            c=[x1 x2 x3 x4; y1 y2 y3 y4; z1 z2 z3 z4 ];
+            c=[p1, p2, p3, p4]
 
             scatter3(c(1,:), c(2,:), c(3,:));
         end
     end
-    axis([0 480 0 640 0 10]);
-    view([45 45]);
+    axis([-10 10 -10 10 0 10]);
+    view([130 45]);
     xlabel('x');
     ylabel('y');
     zlabel('z');
--- track3D_part1.m
+++ track3D_part1.m
@@ -1,12 +1,17 @@
 function frame = track3D_part1( imgseq1,   cam_params )
     
+    load(cam_params)
     imgs=zeros(480,640,length(imgseq1.rgb));
     imgsd=zeros(480,640,length(imgseq1.rgb));
     
-    for i=1:length(imgseq1.rgb)
-        imgs(:,:,i)=rgb2gray(imread(imgseq1.rgb(i).name));
-        load(imgseq1.depth(i).name);
-        imgsd(:,:,i)=double(depth_array)/1000;
+    for k=1:length(imgseq1.rgb)
+        imgs(:,:,k)=rgb2gray(imread(imgseq1.rgb(k).name));
+        load(imgseq1.depth(k).name);
+        imgsd(:,:,k)=double(depth_array)/1000;
+        Z=double(depth_array(:)')/1000;
+        [v u]=ind2sub([480 640],(1:480*640));
+        t=inv(cam_params.Kdepth)*[Z.*u ;Z.*v;Z];
+        xyz(k).coord=reshape(t,[480,640,3]);
     end
 
     
@@ -14,14 +19,14 @@ function frame = track3D_part1( imgseq1,   cam_params )
     bggray=median(imgs,3);
     
     objects=zeros(480,640,length(imgseq1.rgb));
-    for i=1:length(imgseq1.rgb),
-        imdiff=abs(imgsd(:,:,i)-bgdepth)>.20 ;
-        [gx, gy]=gradient(imgsd(:,:,i));
+    for k=1:length(imgseq1.rgb),
+        imdiff=abs(imgsd(:,:,k)-bgdepth)>.20 ;
+        [gx, gy]=gradient(imgsd(:,:,k));
         g=(gx.^2+gy.^2).^0.5;
         imdiff= imdiff - (g > .2);
         imdiff= imdiff > 0;
         imgdiffiltered=imopen(imdiff,strel('disk',5));
-        objects(:,:,i)=bwlabel(imgdiffiltered);
+        objects(:,:,k)=bwlabel(imgdiffiltered);
     end
     
     %%Isto ainda está em pixeis
@@ -32,17 +37,20 @@ function frame = track3D_part1( imgseq1,   cam_params )
         for l=1:obs
            [x,y]=find(objects(:,:,k)==l );
 
-           if(length(x)>400)
-               frame(k).obj(l).x=x;
-               frame(k).obj(l).y=y; 
+           if(length(x)>10)
+               coords=zeros(3,length(x));
+               for count=1:1:length(x);
+                coords(:,count)=xyz(k).coord(x(count),y(count),:);
+               end
+               frame(k).obj(l).xyz=coords;
+                
 
-               [~, frame(k).obj(l).maxx]=max(x);
-               [~, frame(k).obj(l).maxy]=max(y);
-               [~, frame(k).obj(l).minx]=min(x);
-               [~, frame(k).obj(l).miny]=min(y);   
+               [~, frame(k).obj(l).maxx]=max(coords(1,:));
+               [~, frame(k).obj(l).maxy]=max(coords(2,:));
+               [~, frame(k).obj(l).minx]=min(coords(1,:));
+               [~, frame(k).obj(l).miny]=min(coords(2,:));   
            else
-               frame(k).obj(l).x=nan;
-               frame(k).obj(l).y=nan;
+               frame(k).obj(l).xyz=nan;
            end
         end 
     end
